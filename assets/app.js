ff3a185c0749e2eb70bcc8a81acdb756f19537f8
petrero <petrero@gmail.com>
2021-10-22 12:51:27 +0300

    4.4 Turbo-Friendly JavaScript
      Go remove the script tag and the jQuery loading code.
      Writing JavaScript that you (and Turbo) will Love
      - So... what is the best way to write JavaScript so it works nicely with Turbo Drive? Well... Stimulus of course!
    
      We already know from the first tutorial in this series that if a new data-controller element appears on the page - like data-controller="counter", which powers this contest area up here, its Stimulus controller will always work perfectly, even if that HTML is loaded via Ajax. That is the most powerful part of Stimulus and it works brilliantly with Turbo.
    
      One other lesson is that you should probably remove any JavaScript that you have inside your body element... even though it mostly works. That's because of the potential for the bad behavior that we saw a minute ago. In a little while, we'll talk about external JavaScript - like widgets or analytics - which are often supposed to be added to your body.
    
      But let me be clear about one thing: I do not want you to think about all of this like:
    
        Hey! Turbo is forcing me to write my JavaScript a certain way!
    
      Nope: Turbo is forcing you to write better JavaScript: JavaScript that only needs to be loaded and executed once... and then keeps on working forever, even as new content is loaded onto the page.
    
      So this whole JavaScript topic is definitely the biggest hurdle to using Turbo Drive. Until you have all the JavaScript on your site written properly, things won't work well. But you can fix the JavaScript for just some pages on your site and activate Turbo Drive only for those. Let's see how next and also learn how we're able to put all of our script tags into the head element without hurting page-load performance.

--- assets/app.js
+++ assets/app.js
@@ -10,11 +10,3 @@ import './styles/app.css';
 
 // start the Stimulus application
 import './bootstrap';
-
-import $ from 'jquery';
-
-$(document).ready(() => {
-	console.log('page is ready!')
-})
-
-console.log('script is done!')
