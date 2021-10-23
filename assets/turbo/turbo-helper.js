fbf4a8aab0c579fbace0b72a678ec3ee439fb9ad
petrero <petrero@gmail.com>
2021-10-23 23:12:26 +0300

    16.2.CSS Page Transitions
      Triggering the Fade In Transition
      - Let's do that. Head back to turbo-helper.js. I'm going to paste in two more listener functions. Let's walk through this: we've seen both of these events before.
    turbo:before-render fires right before the new body is added to the page. This allows us to add the turbo-loading class to the new body before it's added to the page. This will set its opacity to .2 to start: we want it to start faded out.
    
      Then the turbo:render event is triggered right after that new body is added to the page. Here, we want to remove the turbo-loading class. That will set the opacity back to 1... and thanks to the transition, it should happen slowly over 1 second.
    
      But we can't remove the class immediately... we can't just put this line directly here in the listener. Why not? We need the new body to be rendered for at least 1 "frame" with the lower opacity... so with the turbo-loading class. If we remove it immediately - by just putting the line right here - the element will actually start at full opacity with no transition... because it never got the chance to render with the low opacity.
    
      This is why we have this requestAnimationFrame() function. This is a built-in browser function that says:
    
        Hey, once you do render the next frame, please call this function.
    
      This allows the element to be rendered for one frame with the low capacity... and then we remove the class to transition to full opacity. Pretty freaking cool.
    
      Let's try it. Refresh, and... click. Yes! The fade out and fade in transition looks perfect! Yay! Until... we visit a page we've already been to. Woh. That was weird. It... sort of faded in and... then faded in again?
    
      Let's find out what's going on next and use more Turbo smartness to fix it. By the end, we are going to have perfect fade transitions.

--- assets/turbo/turbo-helper.js
+++ assets/turbo/turbo-helper.js
@@ -43,6 +43,19 @@ const TurboHelper = class {
 			// fade out the old body
 			document.body.classList.add('turbo-loading')
 		})
+
+		document.addEventListener('turbo:before-render', (event) => {
+			// when we are *about* to render, start us faded out
+			event.detail.newBody.classList.add('turbo-loading');
+		});
+
+		document.addEventListener('turbo:render', () => {
+			// after rendering, we first allow the turbo-loading class to set the low opacity
+			// THEN, one frame later, we remove the turbo-loading class, which allows the fade in
+			requestAnimationFrame(() => {
+				document.body.classList.remove('turbo-loading');
+			});
+		});
 	}
 
 	closeModal(){
