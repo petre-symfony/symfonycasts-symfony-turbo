e579a57590f1a35e68702bf00425c017f718edd4
petrero <petrero@gmail.com>
2021-10-23 20:58:49 +0300

    10.1. Fixing the Sweetalert Modal
      So... we've crushed the Bootstrap modal problem! But we have the same issue in one other spot. Go back to the homepage, add an item to your cart and go to the cart. Try to remove the item. This cute little dialog is powered by a library called Sweetalert. Once again, if we click back and then forward, it pops up again, which might be ok... if it actually worked! But... it doesn't... because all of its event listeners are gone.
    
      Okay: let's try using Sweetalert's close functionality to tell it to close before the page is snapshotted. To do that, import Swal from 'sweetalert2'.
    
      Then, down inside of the function, if Swal.isVisible() - they have a nice function to check if Sweetalert is visible - then Swal.close().
    
      It's that simple! Or at least... it might be. Let's go try this. Refresh the cart page, hit remove, go back, go forward and... it worked! Wait, I can't scroll... and nothing is clickable! Inspect element anywhere. Uh oh: a Sweetalert backdrop element is still there! It's invisible, but it's blocking the page!
    
      This is the exact same problem we just saw with Bootstrap's modal: the close animation never finishes, so cleanup never happens. The solution is to, once again, tell Sweetalert to close... but without an animation.

--- assets/app.js
+++ assets/app.js
@@ -11,6 +11,7 @@ import './styles/app.css';
 // start the Stimulus application
 import './bootstrap';
 import { Modal } from 'bootstrap'
+import Swal from 'sweetalert2'
 
 document.addEventListener('turbo:before-cache', () => {
 	if (document.body.classList.contains('modal-open')) {
@@ -20,6 +21,10 @@ document.addEventListener('turbo:before-cache', () => {
 		modal._backdrop._config.isAnimated = false
 		modal.hide()
 		modal.dispose()
+	}
+
+	if (Swal.isVisible()){
+		Swal.close()
 	}
 })
 
@@ -49,4 +54,4 @@ document.addEventListener('hidden.bs.modal', () => {
 	}
 
 	meta.remove();
-}); 
+});
