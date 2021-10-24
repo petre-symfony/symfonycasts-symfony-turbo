bef989bb8d9731780bcb4adc33f7716e7598134c
petrero <petrero@gmail.com>
2021-10-24 10:09:57 +0300

    20.2. Turbo Frames: Lazy Frames
      Setting up a Lazy Frame
      - Okay, at your browser, head to the cart page. We're going to talk about the second use-case for Turbo frames first: lazy frames. See this featured product sidebar? Let's pretend that rendering this is kind of a heavy. If we could load it lazily - so via an Ajax call - then the rest of the cart page could load faster because it wouldn't need to do the work of preparing and rendering that section.
    
      To lazily load this, we first need a route and controller that renders the sidebar. Open the template for this page: templates/cart/cart.html.twig. Let's see... this is where we render the featured sidebar. And you can see that it's already isolated into its own template. So all we need to do is create a route & controller that render this template
    
      Let's do that in src/Controller/CartController.php. This top method is the cart page itself. Copy that, paste below, rename it to _cartFeaturedProduct() and change the URL to /cart/_featured. I like to use that _ prefix when something only renders part of a page. Below, instead of rendering cart.html.twig, render _featuredSidebar.html.twig. And... we don't need to pass the cart variable... and so we don't need this CartStorage. Oh, and the route needs a unique name, like _app_cart_product_featured.
    
      Cool. Now, up in the cart action, this will load faster because we can do less work... because we don't need to prepare the addToCartForm or fetch the featuredProduct anymore. We can even remove this argument.
    
      The Custom <turbo-frame> Element
      - We can do all of this because, in the template for this action - cart.html.twig - we're not going to include this sidebar anymore. Instead, we're going to add a Turbo Frame... which is... just a custom HTML element - <turbo-frame> - which always has at least an id attribute that identifies it, like id="cart-sidebar".
    
      PhpStorm highlights this as an unknown tag, but the Turbo library does register it as a custom element.
    
      If we stopped here, this would render an empty <turbo-frame> element on the page... and would do nothing. To make this a "lazy" frame, add a src attribute set to the URL that it should request to get its contents. In this case, that's {{ path() }} then _app_cart_product_featured. Inside the turbo-frame, we can put some loading text: this will show on page load while the Ajax call is being made.
    
      That's it! With any luck, Turbo will see the frame, initiate the Ajax call and pop the response inside. Let's try it! Refresh and watch closely. Woh: the "Loading..." was there for just a second, then it disappeared! Check the console. Error!
    
      Response has no matching <turbo-frame id="cart-sidebar"> element.
    
      Interesting: it made the Ajax call and then looked for a turbo-frame element in the response with the same id as our frame. Why? The answer goes to the core of how Turbo frames work. Let's dive into that next and get this thing working.

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -20,14 +20,24 @@ class CartController extends AbstractController {
 	/**
 	 * @Route("/cart", name="app_cart")
 	 */
-	public function shoppingCart(CartStorage $cartStorage, ProductRepository $productRepository): Response {
+	public function shoppingCart(CartStorage $cartStorage): Response {
+
+		return $this->renderForm('cart/cart.html.twig', [
+			'cart' => $cartStorage->getOrCreateCart()
+		]);
+	}
+
+	/**
+	 * @Route("/cart/_featured", name="_app_cart_product_featured")
+	 */
+	public function _cartFeaturedProduct(ProductRepository  $productRepository): Response
+	{
 		$featuredProduct = $productRepository->findFeatured();
 		$addToCartForm = $this->createForm(AddItemToCartFormType::class, null, [
 			'product' => $featuredProduct,
 		]);
 
-		return $this->renderForm('cart/cart.html.twig', [
-			'cart' => $cartStorage->getOrCreateCart(),
+		return $this->renderForm('cart/_featuredSidebar.html.twig', [
 			'featuredProduct' => $featuredProduct,
 			'addToCartForm' => $addToCartForm
 		]);
