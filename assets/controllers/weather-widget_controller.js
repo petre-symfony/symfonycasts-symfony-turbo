dbb03d27a3fb38ad5402fa4d69d02b07123e0ccf
petrero <petrero@gmail.com>
2021-10-24 11:19:54 +0300

    23.4. Reliably Load External JS with Stimulus
      The next step is to move all of this JavaScript into our Stimulus controller. Copy everything and delete the <script> tag entirely.
    
      In the controller, after connect(), paste! That is totally invalid JavaScript... and my build system and editor are freaking out.
    
      Let's turn this into a function called initializeScriptTag(). Copy these three arguments and remove them. Cool.
    
      Up in connect(), instead of logging a cloud, say this.initializeScriptTag() and pass those three arguments.
    
      So... this isn't perfect yet... but it's closer: each time Stimulus sees a matching anchor tag, it's going to run this.
    
      Let's try it. Scroll back up to the top, refresh and... awesome! The fact that this loads means that our Stimulus controller did just execute and add the script tag. If you look in the head of our page... there it is!
    
      But... if we scroll to the bottom of the page... that still doesn't work. It's ok, we expected that: we still need to move the __weatherwidget_init() code into Stimulus.

--- assets/controllers/weather-widget_controller.js
+++ assets/controllers/weather-widget_controller.js
@@ -3,6 +3,16 @@ import { Controller } from 'stimulus';
 
 export default class extends Controller {
 	connect() {
-		console.log('🌦')
+		this.initializeScriptTag(document, 'script', 'weatherwidget-io-js')
+	}
+
+	initializeScriptTag(d, s, id) {
+		var js, fjs = d.getElementsByTagName(s)[0];
+		if (!d.getElementById(id)) {
+			js = d.createElement(s);
+			js.id = id;
+			js.src = 'https://weatherwidget.io/js/widget.min.js';
+			fjs.parentNode.insertBefore(js, fjs);
+		}
 	}
 }
