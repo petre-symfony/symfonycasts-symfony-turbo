91a95d0d81d47f9435b16a8ba49845e17568fcf0
petrero <petrero@gmail.com>
2021-10-29 23:36:41 +0300

    36.2. Full Page Redirect from a Frame
      "Binding" this for a Listener Method
    
      - I'm pretty happy with this, but let's clean things up a bit. Copy the code inside the arrow function, scroll down, and create a new method called beforeFetchResponse() with an event argument. I'm doing this for readability.
    
      In connect(), call that. We don't even need an arrow function: just reference this.beforeFetchResponse.
    
      There is a problem with this... but let's try it! Refresh, go back to the admin page, open up the modal and fill this out with real data. Submit!
    
      It didn't redirect! And we have that error back in the console. What happened? It's not super obvious at first, but in our new method, the this variable is no longer referencing the controller object. This is the classic problem with callback functions, and we normally work around "this" by passing an arrow function. But if you do want to point directly to the method, you can by binding the method.

--- assets/controllers/modal-form_controller.js
+++ assets/controllers/modal-form_controller.js
@@ -7,22 +7,23 @@ export default class extends Controller {
 	modal = null;
 
 	connect() {
-		document.addEventListener('turbo:before-fetch-response', event => {
-			console.log(event)
-			if (!this.modal || !this.modal._isShown) {
-				return
-			}
-
-			const fetchResponse = event.detail.fetchResponse
-			if (fetchResponse.succeeded && fetchResponse.redirected){
-				event.preventDefault()
-				Turbo.visit(fetchResponse.location)
-			}
-		})
+		document.addEventListener('turbo:before-fetch-response', this.beforeFetchResponse)
 	}
 
 	async openModal(event) {
 		this.modal = new Modal(this.modalTarget);
 		this.modal.show();
+	}
+
+	beforeFetchResponse(event) {
+		if (!this.modal || !this.modal._isShown) {
+			return
+		}
+
+		const fetchResponse = event.detail.fetchResponse
+		if (fetchResponse.succeeded && fetchResponse.redirected){
+			event.preventDefault()
+			Turbo.visit(fetchResponse.location)
+		}
 	}
 }
