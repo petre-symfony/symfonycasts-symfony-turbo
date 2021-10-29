ad5c55777e30fd40e33e646f48cfe39da6a1a418
petrero <petrero@gmail.com>
2021-10-29 23:10:10 +0300

    34.2. Close the Modal after turbo-frame Success
      turbo:submit-end in Stimulus
      - But in this case, we want to listen to an event only when a specific controller is active... so we can run some custom code that affect just that controller. Say this.element.addEventListener() and listen to an event called turbo:submit-end. Pass this an arrow function with an event argument.
    
      Earlier we listened to turbo:submit-start. As you can see, there is also a turbo:submit-end event, which happens after the submit Ajax call has finished. Let's console.log(event) to see what it looks like.
    
      Oh, and you probably noticed one big difference between this event and the other events that we've listened to. Most Turbo events are dispatched directly on document. But the form events - like turbo:submit-start and turbo:submit-end - are actually dispatched on the form element. Then, they bubble up.
    
      This means that you can attach a listener to document... or any element that contains the form, including the form itself. By attaching the event listener to this.element, our callback will only be executed when a form is submitted inside of this: so inside of the modal. That's... pretty awesome.

--- assets/controllers/modal-form_controller.js
+++ assets/controllers/modal-form_controller.js
@@ -5,6 +5,12 @@ export default class extends Controller {
 	static targets = ['modal'];
 	modal = null;
 
+	connect() {
+		this.element.addEventListener('turbo:submit-end', event => {
+			console.log(event)
+		})
+	}
+	
 	async openModal(event) {
 		this.modal = new Modal(this.modalTarget);
 		this.modal.show();
