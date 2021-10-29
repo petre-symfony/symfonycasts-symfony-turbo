ceb9b7dbcd06ecc4fa79fc97dba1e7f25c036178
petrero <petrero@gmail.com>
2021-10-29 23:02:26 +0300

    33.3. Lazy Modal & Big Cleanup
      Big Ol' Cleanup
      - At this point, if you look at the modal-form controller, its only job is to... open the modal! The turbo-frame inside handles the rest... and that's pretty cool. Let's cleanup a few more things: we don't need useDispatch anymore: we're not dispatching any events... whoops. And... we don't need to import useDispatch or jQuery... and we can also delete the formUrl value.
    
      Cool. In the template for the product index page, we still do need the modal-form controller but we do not need to pass in the formUrl variable.
    
      Above this, we have some fanciness with the reload-content controller. That helped us reload the product list via Ajax after the modal closed. We're going to completely replace that with something simpler in a few minutes. So delete all of that stuff.
    
      Finally, near the bottom, remove this target, which was for the reload-content controller.
    
      Honestly, I'm wondering if it might have been easier to start this feature from scratch! Because most of the work we just did was deleting and simplifying.
    
      Let's make sure we didn't break anything. Refresh, open the modal and submit the form empty. That feels great!
    
      But what happens on a successful form submit? Fill in a title, price and... go! Woh. That's... interesting. It says "loading". Next, let's figure out what just happened. And then, we'll code up the real solution: after a successful form submit, we want to close the modal and reload the list behind us. We're about to bend the frame system to our will!

--- assets/controllers/modal-form_controller.js
+++ assets/controllers/modal-form_controller.js
@@ -1,18 +1,9 @@
 import {Controller} from 'stimulus';
 import {Modal} from 'bootstrap';
-import $ from 'jquery';
-import {useDispatch} from 'stimulus-use';
 
 export default class extends Controller {
 	static targets = ['modal'];
-	static values = {
-		formUrl: String,
-	}
 	modal = null;
-
-	connect() {
-		useDispatch(this);
-	}
 
 	async openModal(event) {
 		this.modal = new Modal(this.modalTarget);
