8263004586aee88d8ea6f24ea96b83c28867f9d5
petrero <petrero@gmail.com>
2021-11-10 22:22:05 +0200

    46.1. Processing Streams by Hand for Fun & Profit
      As we've learned, each time Turbo makes an Ajax call, it listens to see if the response has a content type of text/vnd.turbo-stream.html. If it does, then the HTML is passed to the Turbo Stream system... and it works its magic. But in theory, you could grab some Turbo Stream HTML from anywhere and tell the Stream system to process it. And... it's kind of fun!
    
      Head to the homepage. This counter area is fueled by a Stimulus controller: the one at assets/controllers/counter_controller.js. It's pretty simple: click it and then it increments a variable and updates the text with the new number. In addition to doing this, I want to invent a Turbo Stream that adds a flash message to the top of the page.
    
      Adding the Stream Target to the Page
      - First, we need to be able to target the flash area so that we can put stuff into it. In templates/base.html.twig, find the section - I'll search for flash - and surround it: a <div> with id set to, how about, flash-container. Pop the closing tag on the other side.
    
      Manually Creating a Stream
      - Back in counter_controller.js, right after we update the count on the page, let's add a new variable: const streamMessage set to some ticks so we can easily create a multi-line string. Inside, we're literally going to invent a new <turbo-stream> with action="update" and target="flash-container". Add the template element and, inside of that, an alert success div:
    
        thanks for clicking ${this.count} times.
    
      This variable is a plain, boring string... but a string that has the <turbo-stream> format.
    
      So... could we tell the Turbo Stream system to read this and follow its instructions? And if so, how?
    
      At the top of this file, we're already importing the visit function from @hotwired/turbo. This library exports a bunch of other things, including a function called renderStreamMessage.
    
      Copy that. Down below, it's as simple as this: renderStreamMessage() passing streamMessage.
    
      Done! Let's try this thing. Head back over, refresh and click. Oh! That's so cool. We now have a dead-simple way to mutate different elements on your page from JavaScript. And more importantly, this shows off the fact that stream handling is a standalone system inside of Turbo. And so, in theory, we could get this stream HTML from any source, not just from an Ajax call. That will be important in the next chapter.

--- assets/controllers/counter_controller.js
+++ assets/controllers/counter_controller.js
@@ -1,5 +1,5 @@
 import {Controller} from 'stimulus';
-import { visit } from '@hotwired/turbo';
+import { visit, renderStreamMessage } from '@hotwired/turbo';
 
 export default class extends Controller {
 	count = 0;
@@ -8,6 +8,17 @@ export default class extends Controller {
 	increment() {
 		this.count++;
 		this.countTarget.innerText = this.count;
+
+		const streamMessage = `
+		  <turbo-stream action="update" target="flash-container">
+		  	<template>
+		  		<div class="alert alert-success">
+		  			Thanks for clicking ${this.count} times!
+					</div>
+		  	</template>
+			</turbo-stream>
+		`
+		renderStreamMessage(streamMessage)
 
 		if (this.count === 10){
 			visit('/you-won');
