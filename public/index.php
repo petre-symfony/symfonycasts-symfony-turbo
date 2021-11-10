46c9ced55c899abd35b418944fff10b89133e521
petrero <petrero@gmail.com>
2021-11-10 22:47:34 +0200

    48.2. Running the Mercure Service in the symfony Binary
      The Environment Variables
      - To communicate with this, head back over to your editor and open the .env file.
    
        lines  .env
        ###> symfony/mercure-bundle ###
        # See https://symfony.com/doc/current/mercure.html#configuration
        # The URL of the Mercure hub, used by the app to publish updates (can be a local URL)
        MERCURE_URL=https://127.0.0.1:8000/.well-known/mercure
        # The public URL of the Mercure hub, used by the browser to connect
        MERCURE_PUBLIC_URL=https://127.0.0.1:8000/.well-known/mercure
        # The secret used to sign the JWTs
       ###
      These three environment variables define values that are used in a new config file: config/packages/mercure.yaml. MERCURE_PUBLIC_URL is the public URL to the Mercure hub that our JavaScript will use to subscribe to messages and MERCURE_URL is the URL that our PHP code will use to publish messages. These are usually the same. MERCURE_SECRET is basically a password that will allow us to publish: more on that later.
    
        9 lines  config/packages/mercure.yaml
        mercure:
          hubs:
            default:
                url: '%env(MERCURE_URL)%'
                public_url: '%env(MERCURE_PUBLIC_URL)%'
                jwt:
                  secret: '%env(MERCURE_JWT_SECRET)%'
                  publish: '*'
      In our case, both URL variables already, by chance, point to the correct URL! Yay! But actually, if you're using the latest version of the Symfony binary... we don't even need these variables in this file! Why? Well, in addition to setting up Mercure for us, the Symfony binary also sets these environment variables automatically to their correct values.
    
      Check it out. Back over in our editor, open public/index.php. Let me close a few things... then open it. Cool. Right after the runtime load, I'll paste in some code.
    
      This looks fancy, but I'm basically dumping the  variable... except only the keys that contain MERCURE. The  variable - among other things - will contain all environment variables. I'm filtering for MERCURE basically... because I don't want to accidentally publish any secret keys from my computer to the internet... as much fun as that would be.
    
      Anyways, this will run before the .env file is loaded, so it will only print real environment variables. Back over on our site, refresh!
    
      Tip
    
      - If you're using the docker-compose.yaml setup described earlier, you will only see 2 environment variables here... which are the only 2 you need anyways.
    
      Yay! We see 4 environment variables including 2 we need! The first one is just a flag that tells us that the Symfony binary is running Mercure... and that last one is there for legacy reasons: we don't need it.
    
      This means that our app is already configured and ready to talk to our Mercure Hub! In production, you'll need to run a real Mercure Hub and set these environment variables manually, however you do that in your hosting environment.
    
      So... we have a Mercure hub running! What does that... mean? Well, it's a central place where some things can listen for messages and other things can publish messages. Next, let's do both of these things: listen to a Mercure topic in JavaScript and publish messages to it, both from the command line - just to see how it works - and from PHP, which is our real goal.

--- public/index.php
+++ public/index.php
@@ -4,6 +4,10 @@ use App\Kernel;
 
 require_once dirname(__DIR__) . '/vendor/autoload_runtime.php';
 
+dd(array_filter($_SERVER, function($item) {
+	return str_contains($item, 'MERCURE');
+}, ARRAY_FILTER_USE_KEY));
+
 return function (array $context) {
 	return new Kernel($context['APP_ENV'], (bool)$context['APP_DEBUG']);
 };
