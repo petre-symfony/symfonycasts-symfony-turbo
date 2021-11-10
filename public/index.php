f225853281881b24d4ae6f4203ab0bab66cefd73
petrero <petrero@gmail.com>
2021-11-10 22:50:32 +0200

    49.1. Listening & Publishing
      The purpose of Mercure is to have a hub where we can subscribe - or listen - to messages and also publish messages.
    
      Here's our high-level goal, it's three steps. First, set up some JavaScript that listens to a "topic" in Mercure - a topic is like a message key or category. Second, in PHP, publish a message to that topic containing Turbo Stream HTML. And finally, when our JavaScript receives a message, make it pass the Turbo Stream HTML to the stream-processing system. The result? The power to update any part of anyone's page whenever we want to right from PHP. If this doesn't make sense yet, don't worry: we're going to put this into action right now.
    
      But before we jump in, open index.php and remove the dump... so that our site is no longer dead. Excellent.

--- public/index.php
+++ public/index.php
@@ -4,10 +4,6 @@ use App\Kernel;
 
 require_once dirname(__DIR__) . '/vendor/autoload_runtime.php';
 
-dd(array_filter($_SERVER, function($item) {
-	return str_contains($item, 'MERCURE');
-}, ARRAY_FILTER_USE_KEY));
-
 return function (array $context) {
 	return new Kernel($context['APP_ENV'], (bool)$context['APP_DEBUG']);
 };
