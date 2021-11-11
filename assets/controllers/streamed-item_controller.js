2ec6af489da70089361c8449a141211cf711d2fb
petrero <petrero@gmail.com>
2021-11-11 19:57:37 +0200

    56.3. Broadcasting Frontend Changes on Entity Update/Remove
      Fading out on Remove
      - So let's get fancier. What if, when a review is deleted, instead of instantly disappearing, the element turned red, then faded out. OoooOOOoo.
    
      Start in styles/app.css. Add a new streamed-removed-item class that sets the background-color to coral.
    
      Back in Review.stream.html.twig, this will be a bit trickier. We don't actually want to remove the element anymore... we want to keep it... but trigger some JavaScript that will fade it out.
    
      To do this, change the action to replace... and then copy the entire template from update. But this time, pass in a new variable: isRemoved set to true. We can use that in the template to do something special.
    
      Go open it up: _review.html.twig. If we pass in an isNew variable, we already have code to activate a Stimulus controller that causes the item to get a green background that fades out. We're going to do something similar.
    
      If isRemoved, then initialize that same Stimulus controller. But this time pass className set to streamed-removed-item. This is why we made that controller dynamic. Also pass in another value called removeElement set to true.
    
      This will signal to the controller that we want to fade out the element entirely.
    
      Let's get to work in that file: streamed-item_controller.js.
    
      Start by setting up the removeElement value, which will be a Boolean.
    
      Then, import a helper function called addFadeTransition. This is a utility that we created in the first tutorial to help us fade in or fade out an element.
    
      To activate it, inside connect(), call addFadeTransition() and pass it this object, this.element - the element that we're going to fade - and also an options object with transitioned set to true. That's needed because our element will start visible and then we want it to fade out. If you want to know more about how this all works, check out our Stimulus tutorial.
    
      Inside setTimeout(), check to see if this.removeElementValue is true. If it is not, then keep the original code: this is where we fade out the background color. But if it is true, call this.leave(). That will trigger the entire element to fade out.
    
      Phew! Let's see this thing in action! Go back and find this review... here it is. Refresh the frontend to get the fresh CSS... delete the review... and go to the frontend! Yes! It's there but with a red background! And then... woohoo! It faded out!
    
      The big takeaway here? By combining Turbo Streams with Stimulus, you can do much more than simply "update the HTML of an element". You can do... anything.
    
      Okay team: there's just one more thing that I want to try: using Turbo Streams to pop up "toast" notifications on the frontend, like after we do something awesome. That's next.

--- assets/controllers/streamed-item_controller.js
+++ assets/controllers/streamed-item_controller.js
@@ -1,16 +1,26 @@
-import {Controller} from 'stimulus';
+import { Controller } from 'stimulus'
+import { addFadeTransition } from '../util/add-transition'
 
 export default class extends Controller {
 	static values = {
-		className: String
+		className: String,
+		removeElement: Boolean
 	}
 
 	connect() {
+		addFadeTransition(this, this.element, {
+			transitioned: true
+		})
+
 		this.element.classList.add(this.classNameValue)
 
 		setTimeout(() => {
-			this.element.classList.add('fade-background')
-			this.element.classList.remove(this.classNameValue)
+			if (this.removeElementValue) {
+				this.leave()
+			} else {
+				this.element.classList.add('fade-background')
+				this.element.classList.remove(this.classNameValue)
+			}
 		}, 5000)
 	}
 }
