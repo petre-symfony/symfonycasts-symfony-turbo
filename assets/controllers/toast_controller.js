cc687c714803d498b0ed6fc491f21594fb502257
petrero <petrero@gmail.com>
2021-11-11 21:09:49 +0200

    57.7. Toast Notifications
      In other words, the connect() method will be called twice: once when we originally render our toast element onto the page and again after we move into toast-container. Right now, that's going to cause an infinite loop where we call appendChild() over and over again.
    
      To avoid that, add, if this.element.parentNode does not equal toastContainer. So only if the element has not been moved yet, move it... and then return. The first time this executes, it will move the element and exit. The second time it executes, it will skip all of this and pop open the toast.
    
      Let's try this thing! Refresh the page, add another review and... beautiful! If you quickly inspect the toast element... yup! It lives down inside of toast-container.

--- assets/controllers/toast_controller.js
+++ assets/controllers/toast_controller.js
@@ -4,8 +4,12 @@ import { Toast } from 'bootstrap'
 export default class extends Controller {
 	connect() {
 		const toastContainer = document.getElementById('toast-container')
-		toastContainer.appendChild(this.element)
-		
+		if (this.element.parentNode !== toastContainer) {
+			toastContainer.appendChild(this.element)
+
+			return
+		}
+
 		const toast = new Toast(this.element)
 		toast.show()
 	}
