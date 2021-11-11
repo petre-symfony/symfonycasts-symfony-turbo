55b87cc1ea2ec8d279438428062fcc6898f40be3
petrero <petrero@gmail.com>
2021-11-11 21:00:44 +0200

    57.6. Toast Notifications
      Check it out: at the top of connect(), add const toastContainer = document.getElementById() and pass it toast-container to find the element that lives at the bottom of the page. Then... let's move ourselves into that: toastContainer.appendChild(this.element).
    
      And now that it lives inside the container, we open it like normal!
    
      Though... there is one subtle "catch". When the toast HTML initially loads, it will live here in the middle of the page. Naturally, Stimulus notices this element, instantiates a new controller instance and calls connect(). Yay! But when we move this.element into toast-container, Stimulus destroys the original controller instance, creates a new one, and calls connect() a second time.

--- assets/controllers/toast_controller.js
+++ assets/controllers/toast_controller.js
@@ -3,6 +3,9 @@ import { Toast } from 'bootstrap'
 
 export default class extends Controller {
 	connect() {
+		const toastContainer = document.getElementById('toast-container')
+		toastContainer.appendChild(this.element)
+		
 		const toast = new Toast(this.element)
 		toast.show()
 	}
